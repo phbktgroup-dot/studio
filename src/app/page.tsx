9d822a8ad057fe17ab2223e8fbab101be30aa51b
Hari Garad <phbktgroup@gmail.com>
2026-01-31 10:31:25 +0000

    Our Recent Work move upper side and Perspectives & Insights move below

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
         <HeroSection />
         <PremiumServicesSection />
         <SuccessRoadmapSection />
-        <PerspectivesSection />
         <OurWorkSection />
+        <PerspectivesSection />
         <CareersSection />
         <ContactActionSection />
       </main>
