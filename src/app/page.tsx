a1c8fc701c1e2877ccf923dc2cfb588aabea5806
Hari Garad <phbktgroup@gmail.com>
2026-01-31 10:30:04 +0000

    ESG & Sustainability and Happy Clients
    1200+
    
    Projects Completed
    
    99%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 
 import Header from "@/components/shared/header";
 import HeroSection from "@/components/sections/hero-section";
-import StatsSection from "@/components/sections/stats-section";
 import SuccessRoadmapSection from "@/components/sections/success-roadmap-section";
 import PremiumServicesSection from "@/components/sections/premium-services-section";
 import ContactActionSection from "@/components/sections/contact-action-section";
 import Footer from "@/components/shared/footer";
 import PerspectivesSection from "@/components/sections/perspectives-section";
-import ESGSection from "@/components/sections/esg-section";
 import CareersSection from "@/components/sections/careers-section";
 import OurWorkSection from "@/components/sections/our-work-section";
 
@@ -21,9 +19,7 @@ export default function Home() {
         <SuccessRoadmapSection />
         <PerspectivesSection />
         <OurWorkSection />
-        <ESGSection />
         <CareersSection />
-        <StatsSection />
         <ContactActionSection />
       </main>
       <Footer />
